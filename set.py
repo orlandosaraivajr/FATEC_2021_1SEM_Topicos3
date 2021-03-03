2a52038ab2bd29550483f342cf9f845452ec3077
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-03-02 21:14:11 -0300

    Desafio do SET resolvido

--- set.py
+++ set.py
@@ -3,3 +3,11 @@ lista2 = [('aluno4',40), ('aluno2',20), ('aluno3',30)]
 lista3 = [('aluno4',40), ('aluno2',20), ('aluno5',50)] 
 
 #quais são os registros comuns a todas as listas ?
+
+set1 = set(lista1)
+set2 = set(lista2)
+set3 = set(lista3)
+
+set1.intersection(set2, set3) 
+
+set1.intersection(set2).intersection(set3) 
