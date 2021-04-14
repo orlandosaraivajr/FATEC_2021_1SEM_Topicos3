be6379d1ba923f8da1050f635bf78e88911b4bb9
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-04-13 22:15:59 -0300

    Projeto Feriado v.04

--- projeto/feriados/feriado/tests.py
+++ projeto/feriados/feriado/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from feriado.models import FeriadoModel
+from datetime import datetime
 
 class NatalTest(TestCase):
     def setUp(self):
@@ -12,3 +14,26 @@ class NatalTest(TestCase):
 
     def test_template_natal(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+            nome=self.feriado,
+            dia=self.dia,
+            mes=self.mes,
+        )
+        self.cadastro.save()
+
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
