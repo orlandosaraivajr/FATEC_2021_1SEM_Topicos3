de5ddf5c6e11fc5c27aae22077ffbc31be9767f3
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-04-27 20:17:16 -0300

    Revisão das aulas anteriores

--- projeto/feriados/feriado/tests.py
+++ projeto/feriados/feriado/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from feriado.models import FeriadoModel
 from datetime import datetime
 
-class NatalTest(TestCase):
+class TestView_SemFeriado_Test(TestCase):
     def setUp(self):
         self.resp = self.client.get('/')
 
@@ -10,10 +10,34 @@ class NatalTest(TestCase):
         self.assertEqual(200, self.resp.status_code)
 
     def test_texto(self):
-        self.assertContains(self.resp, 'natal')
+        self.assertContains(self.resp, 'Feriado')
+        self.assertContains(self.resp, 'Não é feriado')
 
-    def test_template_natal(self):
-        self.assertTemplateUsed(self.resp, 'natal.html')
+    def test_template_mostra_feriado(self):
+        self.assertTemplateUsed(self.resp, 'mostra_feriado.html')
+
+from datetime import date
+class TestView_ComFeriado_Test(TestCase):
+    def setUp(self):
+        hoje = date.today()
+        self.cadastro = FeriadoModel(
+            nome='Dia do Saci Pererê',
+            dia=hoje.day,
+            mes=hoje.month,
+        )
+        self.cadastro.save()
+        self.resp = self.client.get('/')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'Feriado')
+        self.assertContains(self.resp, 'Dia do Saci Pererê')
+
+    def test_template_mostra_feriado(self):
+        self.assertTemplateUsed(self.resp, 'mostra_feriado.html')
+
 
 
 class FeriadoModelTest(TestCase):
--- projeto/feriados/feriado/views.py
+++ projeto/feriados/feriado/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from feriado.models import FeriadoModel
+from datetime import date
 
-def natal(request):
-    contexto = {'natal':False,
-                'carnaval':False}
-    return render(request, 'natal.html', contexto)
+def feriado(request):
+    verifica_feriado = False
+    nome_feriado = ''
+    hoje = date.today()
+    feriados = FeriadoModel.objects.all()
+    for feriado in feriados:
+        if feriado.dia == hoje.day:
+            if feriado.mes == hoje.month:
+                verifica_feriado = True
+                nome_feriado = feriado.nome
+    contexto = {'feriado':verifica_feriado,
+                'nome_feriado':nome_feriado}
+    return render(request, 'mostra_feriado.html', contexto)
